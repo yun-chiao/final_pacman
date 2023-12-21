005f15f6d85a573d2b6f3e0af8a7f8afefd973ca
b08902043 <56215453+b08902043@users.noreply.github.com>
2023-12-22 04:45:01 +0800

    Add files via upload

---
+++ Assets/RandomCoin.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class RandomCoin : MonoBehaviour
+{
+        public GameObject coin1;
+        public GameObject coin2;
+        public GameObject coin3;
+        public GameObject coin4;
+        public int numberOfCoins = 1;
+
+        void Start()
+        {
+            //first coin
+            
+            createCoin(-24, -10, 10, 24, coin1);
+            createCoin(-22, -13, -10, -19, coin2);
+            createCoin(13, 22, 10, 24, coin3);
+            createCoin(13, 22, -24, -10, coin4);
+
+        }
+        void createCoin(int x1, int x2, int z1, int z2, GameObject coin)
+        {
+            int x;
+            int z;
+            bool success = false;
+            while (!success)
+            {
+                x = Random.Range(x1, x2);
+                z = Random.Range(z1, z2);
+                Vector3 checkPosition = new Vector3(x, 3f, z);
+                Ray ray = new Ray(checkPosition, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.CompareTag("ground"))
+                    {
+                        checkPosition = new Vector3(x, 0.5f, z);
+                        coin.transform.position = checkPosition;
+                        success = true;
+                    }
+                }
+            }
+        }
+    }
