0899f83c4b0b542cf9bdca616b861ac5a302fda6
b08902043 <56215453+b08902043@users.noreply.github.com>
2023-12-24 15:51:54 +0800

    Modify start scene

--- Assets/Scripts/StartGetPacPos.cs
+++ Assets/Scripts/StartGetPacPos.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StartGetPacPos : MonoBehaviour
 {
     public OSC osc;
-    public GameObject canvas;
+    public RectTransform canvasRectTransform;
     // Update is called once per frame
     public float FrameRate = 30f;    // 設定資料更新頻率
     void Start()
@@ -15,11 +15,13 @@ public class StartGetPacPos : MonoBehaviour
     void FixedUpdate()
     {
         OscMessage message;
+        float canvasSize = canvasRectTransform.rect.width * canvasRectTransform.localScale.x;
         //Todo: /source/7/aed float 1 float (bgm位置 水平角度 縱向角度 距離, 更新速度0.1s)
         //Angle: 開始畫面中小精靈位置和觀眾夾角
-        float x = transform.position.x - 800 / 2;
-        float z = 800 / 2;
+        float x = transform.position.x - canvasSize / 2;
+        float z = canvasSize / 2;
         float Angle = Mathf.Atan2(z,x) * Mathf.Rad2Deg -90;
+        Debug.Log(Angle);
         float distance = Mathf.Sqrt(z*z+x*x);
         message = new OscMessage();
         message.address = "/source/7/aed";
--- Assets/Scripts/StartUIMovement.cs
+++ Assets/Scripts/StartUIMovement.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class StartGhostMovement : MonoBehaviour
 {
-    public float moveSpeed = 60f;
-
+    public RectTransform canvasRectTransform;
     void Update()
     {
         Vector3 movement = new Vector3(-1f, 0, 0);
         Vector3 pos = transform.position;
-        if(pos.x < 100)
+        float width = canvasRectTransform.rect.width;
+        float scale = canvasRectTransform.localScale.x;
+        float moveSpeed = 200f * scale;
+        if (pos.x < 0)
         {
-            pos.x = 800;
+            pos.x = width * scale;
             transform.position = pos;
         }
         else
